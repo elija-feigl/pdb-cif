a7fdb67e0f7ac5e209d445bc906582a0a01509ea
elija-feigl <elija.feigl@gmail.com>
2020-12-18 10:15:36 +0100

    feature (cif): * compute chains and sequences * add squence info to output * add chain info to output ! tested against chimeraX & validation server

--- core/files.py
+++ core/files.py
@@ -6,7 +6,7 @@ from __future__ import annotations
 import attr
 
 from pathlib import Path
-from typing import List, TextIO, TYPE_CHECKING
+from typing import List, TextIO, TYPE_CHECKING, Dict, Tuple
 if TYPE_CHECKING:
     from core.structure import Structure
 try:
@@ -16,6 +16,7 @@ except ImportError:
     import importlib_resources as resources
 
 from core import cif_templates
+from core.types import ChainID, ResName
 
 
 @attr.s
@@ -25,7 +26,6 @@ class PDB(object):
     def __attrs_post_init__(self):
         # TODO: get box from struct
         self.box = "1000.000 1000.000 1000.000"
-        # TODO: add additional info
 
     def _header(self) -> List[str]:
         return ["HEADER   "]
@@ -64,14 +64,34 @@ class CIF(object):
 
     def __attrs_post_init__(self):
         self.atoms: List[str] = self._set_atoms()
-        # TODO: add additional info
+        self.chains, self.seqs = self._get_chains_seqs()
 
     def _set_atoms(self) -> List[str]:
         return [atom.asCif() for atom in self.struct.atoms]
 
+    def _get_chains_seqs(self) -> Tuple[
+            Dict[int, int], Dict[int, List[ResName]]]:
+        chains: Dict[int, int] = dict()
+        seqs: Dict[int, List[ResName]] = dict()
+        C3ps = [a for a in self.struct.atoms if a.atom_name.as_str() == "C3\'"]
+        for a in C3ps:
+            chain_id = a.chain_id.n
+            res_number = a.res_number.n
+
+            if chain_id not in chains:
+                chains[chain_id] = res_number
+            elif res_number > chains[chain_id]:
+                chains[chain_id] = res_number
+
+            if chain_id not in seqs:  # only res not atom
+                seqs[chain_id] = [a.res_name]
+            else:
+                seqs[chain_id].append(a.res_name)
+
+        return chains, seqs
+
     def _write_atoms(self, fo: TextIO) -> None:
         atom_header = resources.read_text(cif_templates, "atom_header.txt")
-
         fo.write(atom_header)
         fo.writelines(self.atoms)
 
@@ -80,10 +100,50 @@ class CIF(object):
         fo.write("data_{}\n".format(self.struct.name))
         fo.write(header)
 
-    def write(self, outfile: Path) -> None:
-        close_section = "#\n"
+    def _write_pdbx_struct(self, fo: TextIO) -> None:
+        pdbx_struct = resources.read_text(cif_templates, "pdbx_struct.txt")
+        pdbx_struct = pdbx_struct.replace("NCHAINS", str(len(self.chains)))
+        fo.write(pdbx_struct)
 
+    def _write_entity(self, fo: TextIO) -> None:
+        entity = resources.read_text(cif_templates, "entity.txt")
+        fo.write(entity)
+        for chain_id, length in self.chains.items():
+            is_staple = (length < 500)
+            n = str(chain_id).ljust(4)
+            src = "syn" if is_staple else "?  "
+            typ = "\'STAPLE STRAND\'  " if is_staple else "\'SCAFFOLD STRAND\'"
+            fo.write("{} polymer {} {} ?   1 ? ? ? ?\n".format(n, src, typ))
+
+    def _write_entity_src(self, fo: TextIO) -> None:
+        entity_src = resources.read_text(cif_templates, "entity_src.txt")
+        fo.write(entity_src)
+        for chain_id, length in self.chains.items():
+            is_staple = (length < 500)
+            n = str(chain_id).ljust(4)
+            typ = "\'synthetic construct\'" if is_staple else "?".ljust(21)
+            tax = "32630" if is_staple else "?".ljust(5)
+            fo.write("{}   1 sample 1 {} {} ? {} ?\n".format(
+                n, str(length).ljust(5), typ, tax))
+
+    def _write_entity_poly(self, fo: TextIO) -> None:
+        entity_poly = resources.read_text(cif_templates, "entity_poly.txt")
+        fo.write(entity_poly)
+        for chain_id, seq in self.seqs.items():
+            n = str(chain_id).ljust(4)
+            cid = ChainID(chain_id).as_chimera()
+            seq1 = "".join(["({})".format(s.as_str()) for s in seq])
+            seq2 = "".join([s.as_X() for s in seq])
+            fo.write(
+                "{} polydeoxyribonucleotide no no\n;{}\n;\n{} {} ?\n".format(
+                    n, seq1, seq2, cid)
+            )
+
+    def write(self, outfile: Path) -> None:
         with open(outfile, mode="w+") as fo:
             self._write_header(fo)
+            self._write_pdbx_struct(fo)
             self._write_atoms(fo)
-            fo.write(close_section)
+            self._write_entity(fo)
+            self._write_entity_src(fo)
+            self._write_entity_poly(fo)
